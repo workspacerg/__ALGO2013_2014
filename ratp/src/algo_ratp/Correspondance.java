83b9aad4e762eee953383443a8162f916fa8ebe1
Andy <Andy@HpPavilDv7-Andy.home>
2013-12-07 20:06:05 +0100

    renvoi

---
+++ ratp/src/algo_ratp/Correspondance.java
@@ -0,0 +1,27 @@
+package algo_ratp;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Correspondance {
+	
+	private Map<Station,LigneCorrespondance> mapLigne;
+	private static Correspondance INSTANCE = null;
+	private Correspondance(){
+		mapLigne = new HashMap<Station, LigneCorrespondance>();
+	}
+	
+	public static Correspondance getInstance(){
+		if(INSTANCE == null){
+			INSTANCE = new Correspondance();
+		}
+		
+		return INSTANCE;
+	}
+
+	public Map<Station,LigneCorrespondance> getMapLigne() {
+		return mapLigne;
+	}
+	
+}
