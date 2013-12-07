b61f5569fbc2aaf322b8a24a2778c8a4d8231da6
Rayane Rahmani <rayane.rahmani@hotmail.fr>
2013-12-07 20:12:08 +0100

    t

---
+++ ratp/src/algo_ratp/ComparatorStation.java
@@ -0,0 +1,12 @@
+package algo_ratp;
+
+import java.util.Comparator;
+
+public class ComparatorStation implements Comparator<Station> {
+
+        @Override
+        public int compare(Station arg0, Station arg1) {
+                return Integer.compare(arg0.getSequence(),arg1.getSequence());
+        }
+
+}
